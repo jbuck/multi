fac1e44222d24c94b466415dc8bce531b18f32cc
Jen Fong-Adwent <jen@ednapiranha.com>
2013-02-14 19:27:24 -0500

    fixed typo; adjusted canvas

--- main.js
+++ main.js
@@ -1,18 +1,24 @@
-var canvas = document.getElementByTagName('canvas');
+var canvas = document.getElementById('canvas');
 var ctx = canvas.getContext('2d');
 
-ctx.strokeStyle = '#367dbc';
-ctx.translate(-.5,-.5);
+document.body.addEventListener('touchmove', function (event) {
+  event.preventDefault();
+}, false);
 
 canvas.addEventListener('touchstart', function (event) {
   for (var i = 0; i < event.touches.length; i ++) {
     var touch = event.touches[i];
+
     ctx.beginPath();
-    ctx.arc(touch.pageX, touch.pageY, 20, 0, 2*Math.PI, true);
+    ctx.arc(px, py, 20, 0, 2 * Math.PI, true);
+
+    ctx.fillStyle = '#f00';
     ctx.fill();
+    ctx.lineWidth = 2.0;
+    ctx.strokeStyle = '#367dbc';
     ctx.stroke();
   }
 }, false);
 
-canvas.addEventListener("touchmove", function () {}, false);
-canvas.addEventListener("touchend", function () {}, false);
+canvas.addEventListener('touchmove', function () {}, false);
+canvas.addEventListener('touchend', function () {}, false);
